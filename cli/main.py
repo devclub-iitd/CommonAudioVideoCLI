274a06602dfaed4518b39f85576360d40e4ed869
Harsh Agrawal <harsh.ag14901@gmail.com>
2020-05-13 17:05:34 +0530

    Parsing Logs to get information

--- cli/main.py
+++ cli/main.py
@@ -2,29 +2,34 @@ import argparse
 from audio_extract import extract
 from vlc_comm import VLC_instance
 import time
-from multiprocessing import Process , Pool
+from multiprocessing import Process, Pool
 from itertools import product
 import vlc_comm
 import util
 
-args=None
-def parse():
-    parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group()
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
 
-    parser.add_argument('-f','--file',required=True,dest="f",help="Path to video file",type=str,action="append")
-    parser.add_argument('-s','--sub',dest="sub",help="Load subtitle File",type=str,action="store")
-    parser.add_argument('--qr',help="Show qr code with the link",dest="qr",action="store_true")
-    parser.add_argument('--audio-quality',dest="q",help="Audio quality to sync from",choices=["low","medium","good","high"],type=str,default="medium")
+parser.add_argument('-f', '--file', required=True, dest="f",
+                    help="Path to video file", type=str, action="append")
+parser.add_argument('-s', '--sub', dest="sub",
+                    help="Load subtitle File", type=str, action="store")
+parser.add_argument(
+    '--qr', help="Show qr code with the link", dest="qr", action="store_true")
+parser.add_argument('--audio-quality', dest="q", help="Audio quality to sync from",
+                    choices=["low", "medium", "good", "high"], type=str, default="medium")
 
-    group.add_argument('--local',help="Host locally",dest="local",action="store_true")
-    group.add_argument('--web',help="Route through a web server",dest="web",action="store_true")
+group.add_argument('--local', help="Host locally",
+                   dest="local", action="store_true")
+group.add_argument('--web', help="Route through a web server",
+                   dest="web", action="store_true")
 
-    args=parser.parse_args()
+args = parser.parse_args()
 
 
 def send_to_server(name):   # TO be implemented in server_comm.py
     print(f"Files ..{name}.. sending to server")
+
 
 def convert_async():
     pool = Pool()
@@ -37,26 +42,21 @@ def convert_async():
     print(
         f"Completed execution of {len(args.f)} processes in {time.perf_counter()-st} seconds")
 
-#init server conf.... create room
 
-# player = VLC_instance(1234)
-# player.launch()
-# Process(target=player.update).start()
+######################################
 
-# pos=60
-# for file_path in args.f:
-#     player.enqueue(file_path)
-#     player.play()
-#     time.sleep(0.5)
-#     player.seek(60)
-#     # time.sleep(0.1)
-#     print(player.getState())
-#     while(True):
-#         print(player.getState())
-#         time.sleep(5)
-#         pos+=20
-#         player.seek(pos)
-#         time.sleep(0.1)
 
-if __name__ == "__main__":
-    parse()
+time.sleep(1)
+player = VLC_instance(1234)
+player.launch()
+Process(target=player.update).start()
+for file_path in args.f:
+    print(args.f)
+    player.enqueue(file_path)
+    time.sleep(2)
+    player.play()
+    # for line in iter(player.proc.stdout.readline,""):
+    #     print(line)
+    while(True):
+        print(player.getState())
+        time.sleep(1)
